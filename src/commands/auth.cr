c8df717c203744fff2e51464a03716d52d0cc29c
kkpagaev <kkpagaev@gmail.com>
2023-04-02 03:10:57 +0300

    init with auth

---
+++ src/commands/auth.cr
@@ -0,0 +1,54 @@
+class Auth < Command::Base
+  property email : String = ""
+  property password : String = ""
+  def execute
+    get_token
+  end
+
+  private def get_token
+    OptionParser.parse do |parser|
+      default_options parser
+
+      parser.on("-e", "--email EMAIL", "Email") do |email|
+        @email = email
+      end
+
+      parser.on("-p", "--password PASSWORD", "Password") do |password|
+        @password = password
+      end
+    end
+
+    if @email.empty? || @password.empty?
+      puts "Email and password are required"
+      exit(1)
+    end
+
+    puts "Authenticating..."
+    res = Crest.post(
+      "http://127.0.0.1:3000/api/signin",
+      {:email => email, :password => password}
+    )
+    hash = Hash(String, String).from_json(res.body)
+    if hash.has_key?("error")
+      puts "Authentication failed"
+      puts "Error: #{hash["error"]}"
+      exit(1)
+    elsif hash.has_key?("token")
+      puts "Authentication successful"
+      token = hash["token"]
+      save_token token
+    else
+      puts "Unknown error"
+      exit(1)
+    end
+  end
+
+  private def save_token(token)
+    # check if dir exists
+    dir = "#{ENV["HOME"]}/.config/polaris"
+    Dir.mkdir(dir) unless Dir.exists?(dir)
+
+    puts "Saving token to ~/.config/polaris/token"
+    File.write(dir + "/token", token)
+  end
+end
--- src/moon-cli.cr
+++ src/moon-cli.cr
@@ -1,6 +1,65 @@
-# TODO: Write documentation for `Moon::Cli`
-module Moon::Cli
-  VERSION = "0.1.0"
+require "./command"
+require "./commands/*"
 
-  # TODO: Put your code here
+require "option_parser"
+require "moon-markdown"
+require "crest"
+require "json"
+
+class Params
+  property date : Time = Time.utc
 end
+
+params = Params.new
+
+def default_options(parser)
+  parser.on "-v", "--version", "Show version" do
+    puts "version 0.1"
+    exit
+  end
+
+  parser.banner = "Usage: polaris [subcommand] [options]"
+
+  parser.on "-h", "--help", "Show help" do
+    puts parser
+    exit
+  end
+
+  parser.missing_option do |option_flag|
+    STDERR.puts "ERROR: #{option_flag} is missing something."
+    STDERR.puts ""
+    STDERR.puts parser
+    exit(1)
+  end
+
+  parser.invalid_option do |option_flag|
+    STDERR.puts "ERROR: #{option_flag} is not a valid option."
+    STDERR.puts parser
+    exit(1)
+  end
+end
+
+
+OptionParser.parse do |parser|
+  default_options parser
+
+  parser.on "auth", "Auth command" do
+    auth = Auth.new
+    auth.execute
+    exit
+  end
+end
+
+def parse(body : String, date : String)
+  parser = MarkdownParser.new body
+  parser.parse_day("28.03.2023")
+end
+
+# Dir.open(Dir.current + "/calendar").each do |file|
+# next if file == "." || file == ".." || File.directory?(file)
+# date = file.gsub(".md", "")
+# puts date
+# body = File.read("calendar/" + file)
+# puts parse(body, date).inspect
+# end
+
